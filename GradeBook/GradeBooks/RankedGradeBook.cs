556a023520a4504a51cb49e1800e903c0c4c371c
Sam Talbot <sam.talbot@fmc-na.com>
2018-08-08 13:39:26 -0600

    Override for GetLetterGrade in RankedGradebook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,5 +11,27 @@ namespace GradeBook.GradeBooks
 		{
 			Type = Enums.GradeBookType.Ranked;
 		}
-    }
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if(Students.Count < 5)
+			{
+				throw new InvalidOperationException();
+			}
+
+			var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+			var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+			if (grades[threshold - 1] <= averageGrade)
+				return 'A';
+			else if (grades[(threshold * 2) - 1] <= averageGrade)
+				return 'B';
+			else if (grades[(threshold * 3) - 1] <= averageGrade)
+				return 'C';
+			else if (grades[(threshold * 4) - 1] <= averageGrade)
+				return 'D';
+			else
+				return 'F';
+		}
+	}
 }
